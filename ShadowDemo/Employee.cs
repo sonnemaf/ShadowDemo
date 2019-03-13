69d0ab857c13f6ce45ef9c74e3b884a46d0acc4b
Fons <Fons.Sonnemans@reflectionit.nl>
2019-03-13 14:06:32 +0100

    Initial version

---
+++ ShadowDemo/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace ShadowDemo {
+
+    internal class Employee {
+        public Employee() {
+        }
+
+        public Employee(string name, double salary) {
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        public string Name { get; set; }
+
+        public void RaiseSalary(double percentage) {
+            this.Salary += Salary * (percentage / 100);
+        }
+
+        public double Salary { get; set; }
+
+        public override string ToString() => $"Employee Name = {this.Name}, Salary = {this.Salary.ToString("C")}";
+    }
+
+}
---
+++ ShadowDemo/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ShadowDemo {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<Employee> Employees { get; } = new List<Employee> {
+                new Employee("Fons", 2000),
+                new Employee("Jim", 4000),
+                new Employee("Ellen", 3000),
+                new Employee("Eric", 1000),
+                new Employee("Eric", 1000),
+                new Employee("Eric", 1000),
+                new Employee("Eric", 1000),
+                new Employee("Eric", 1000),
+            };
+
+
+        public MainPage() {
+            this.InitializeComponent();
+            MyThemeShadow.Receivers.Add(BgRectangle);
+        }
+    }
+}
+
